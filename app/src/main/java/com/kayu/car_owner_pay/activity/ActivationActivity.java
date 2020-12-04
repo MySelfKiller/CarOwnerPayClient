bdfa7f0840598d125c8185d2b29169d1760ab03d
HuangMin <506940294@qq.com>
2020-12-04 18:18:27 +0800

    修改登录页面 添加激活卡功能

---
+++ app/src/main/java/com/kayu/car_owner_pay/activity/ActivationActivity.java
@@ -0,0 +1,14 @@
+package com.kayu.car_owner_pay.activity;
+
+import android.os.Bundle;
+
+import com.kayu.car_owner_pay.R;
+
+public class ActivationActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activation);
+    }
+}
